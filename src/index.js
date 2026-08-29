076433e0a9cdf00e9566a409ee9610acb7293dc4
bonanaf <122012550+bonanaf@users.noreply.github.com>
2026-08-30 00:25:58 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.closeai.me": dockerHub,
-  "quay.closeai.me": "https://quay.io",
-  "gcr.closeai.me": "https://gcr.io",
-  "k8s-gcr.closeai.me": "https://k8s.gcr.io",
-  "k8s.closeai.me": "https://registry.k8s.io",
-  "ghcr.closeai.me": "https://ghcr.io",
-  "cloudsmith.closeai.me": "https://docker.cloudsmith.io",
+  "docker.56565856.xyz": dockerHub,
+  "quay.56565856.xyz": "https://quay.io",
+  "gcr.56565856.xyz": "https://gcr.io",
+  "k8s-gcr.56565856.xyz": "https://k8s.gcr.io",
+  "k8s.56565856.xyz": "https://registry.k8s.io",
+  "ghcr.56565856.xyz": "https://ghcr.io",
+  "cloudsmith.56565856.xyz": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.closeai.me": dockerHub,
+  "docker-staging.56565856.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
